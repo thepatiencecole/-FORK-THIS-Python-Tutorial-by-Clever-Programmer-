58bfd85417eca7a79d086239d027dc10d8edf36d
patience c <96226401+thepatiencecole@users.noreply.github.com>
2022-09-04 07:33:57 +0000

    float

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # tip calculator App
-food_amount = 100
-tip_percentage = 20/100
+food_amount = float(input('enter food amount $: '))
+tip_percentage = float(input('enter tip percentage %:')) / 100
 tip_amount = food_amount * tip_percentage
 total = food_amount + tip_amount
 print('$' + str(total))
